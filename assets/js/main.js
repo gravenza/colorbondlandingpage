41cb3b2b4e060d83b9965a416f363cb59ed93018
gravenza <toniewibowo@gmail.com>
2018-08-30 17:07:42 +0700

    update main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
+  var wWidth = $(window).width();
   var wheight = $(window).height();
+
+  console.log('width:'+ wWidth);
+  console.log('height:'+ wheight);
+
   $('section.form-award').css('height',wheight);
   $('.align-items-center').css('height',(wheight - 300));
 })
