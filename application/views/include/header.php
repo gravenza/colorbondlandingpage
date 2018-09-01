1c228be3f68cc85b39fc7ae4b93746eb0e890c26
gravenza <toniewibowo@gmail.com>
2018-09-01 13:05:56 +0700

    upload jquery validate

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -8,6 +8,7 @@
     <!-- JQUERY & JAVASCRIPT -->
     <script type="text/javascript" src="<?php echo base_url() ?>assets/js/jquery-1.11.3.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>assets/js/jquery-1.11.3.min.js"></script>
+		<script type="text/javascript" src="<?php echo base_url() ?>assets/js/jquery.validate.min.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>assets/js/bootstrap.js"></script>
     <script type="text/javascript" src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url() ?>assets/js/main.js"></script>
