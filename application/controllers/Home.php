13c0efe46cff70278a7f6b9a94a8b42b8d37d8b4
gravenza <toniewibowo@gmail.com>
2018-09-02 23:07:16 +0700

    great award

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -83,13 +83,17 @@ class Home extends CI_Controller
         echo 'alert("Submit Award Success.")';
         echo '</script>';
 
+        $this->load->view('include/header');
+        $this->load->view('home',$data);
+        $this->load->view('include/footer');
+
+        redirect('home');
+
       }
 
-      $this->load->view('include/header');
-      $this->load->view('home',$data);
-      $this->load->view('include/footer');
 
-      redirect('home');
+
+
 
     }
   }
